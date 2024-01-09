249bb5b5c0ad559de018b3979ac61d9f73ca8d48
rans-hekel <ransenju27@gmail.com>
2024-01-09 21:08:26 +0700

    first setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>ref</h2>
+    <p>halo nama saya {{ contactOne.name }} umur saya {{ contactOne.umur }} </p>
+    <button @click="handleClick1">Click!</button><br>
+    <button @click="umur++">tambah umur</button><br>
+    <input type="text" v-model="nama"><br>
+    <h2>reactive</h2>
+    <p>halo nama saya {{ contactTwo.name }} umur saya {{ contactTwo.umur }} </p>
+    <button @click="handleClick2">Click!</button>
   </div>
 </template>
 
 <script>
+import { reactive, ref } from 'vue'
+
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    // const paragraf = ref(null)
+
+ const contactOne = ref({
+   name: 'ranggakun',
+   umur: 24
+ })
+ const contactTwo = reactive({
+   name: 'fu xuan chan',
+   umur: 24
+ })
+    const handleClick1 = () => {
+      contactOne.value.name = 'kun'
+      contactOne.value.umur = 25
+    }
+    const handleClick2 = () => {
+      contactTwo.name = 'chan'
+      contactTwo.umur = 2000
+    }
+
+    return  {
+      contactOne,
+      contactTwo,
+      handleClick1,
+      handleClick2
+    }
+  },
+  
 }
 </script>
+
+
+<style scoped>
+.paragraf {
+  color: red;
+}
+</style>
