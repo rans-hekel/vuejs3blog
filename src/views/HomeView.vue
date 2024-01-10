582b1575dbcdf048d2427e6efaae4433c107882e
rans-hekel <ransenju27@gmail.com>
2024-01-10 16:42:40 +0700

    New Script

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,36 @@
 <template>
   <div class="home">
-    <h2>ref</h2>
-    <p>halo nama saya {{ contactOne.name }} umur saya {{ contactOne.umur }} </p>
-    <button @click="handleClick1">Click!</button><br>
-    <button @click="umur++">tambah umur</button><br>
-    <input type="text" v-model="nama"><br>
-    <h2>reactive</h2>
-    <p>halo nama saya {{ contactTwo.name }} umur saya {{ contactTwo.umur }} </p>
-    <button @click="handleClick2">Click!</button>
+    <input type="text" v-model="search">
+     <div v-for="(name, index) in resultSearch" key="index">
+      <p>Hello {{ name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from 'vue'
-
-// @ is an alias to /src
+import { computed, ref } from 'vue';
 
 
 export default {
   name: 'HomeView',
   setup() {
-    // const paragraf = ref(null)
+    const search = ref('')
+    const names = ref(['John', 'Jane', 'Joe', 'Jill', 'Jack', 'Judy', 'Jude', 'Jenna', 'Jenifer', 'Jen', 'Jenin']) ;
+    const resultSearch = computed(() => {
+      return names.value.filter(name => name.toLowerCase().includes(search.value.toLowerCase()))
+    })
 
- const contactOne = ref({
-   name: 'ranggakun',
-   umur: 24
- })
- const contactTwo = reactive({
-   name: 'fu xuan chan',
-   umur: 24
- })
-    const handleClick1 = () => {
-      contactOne.value.name = 'kun'
-      contactOne.value.umur = 25
-    }
-    const handleClick2 = () => {
-      contactTwo.name = 'chan'
-      contactTwo.umur = 2000
+    return {
+      names,
+      search,
+      resultSearch
     }
 
-    return  {
-      contactOne,
-      contactTwo,
-      handleClick1,
-      handleClick2
-    }
-  },
-  
+  }
 }
 </script>
 
 
 <style scoped>
-.paragraf {
-  color: red;
-}
+
 </style>
